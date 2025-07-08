299401cdc5246e4a596a162a88516cb56cadcdc4
Shreyasgajbhiye <shreyashgajbhiye90@gmail.com>
2025-07-08 18:26:45 +0530

    bugs fixed

--- user-service/src/main/java/com/streamit/user_service/dto/RegisterRequest.java
+++ user-service/src/main/java/com/streamit/user_service/dto/RegisterRequest.java
@@ -1,13 +1,37 @@
+// package com.streamit.user_service.dto;
+
+// import java.util.Set;
+
+// import lombok.Data;
+
+// @Data
+// public class RegisterRequest {
+//     private String username;
+//     private String email;
+//     private String password;
+//     private Set<String> roles;
+// }
+
 package com.streamit.user_service.dto;
 
 import java.util.Set;
 
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class RegisterRequest {
     private String username;
     private String email;
     private String password;
+    private String firstName;
+    private String lastName;
     private Set<String> roles;
 }
--- user-service/src/main/java/com/streamit/user_service/entity/User.java
+++ user-service/src/main/java/com/streamit/user_service/entity/User.java
@@ -1,3 +1,48 @@
+// package com.streamit.user_service.entity;
+
+// import java.util.Set;
+
+// import jakarta.persistence.CollectionTable;
+// import jakarta.persistence.Column;
+// import jakarta.persistence.ElementCollection;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.FetchType;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.GenerationType;
+// import jakarta.persistence.Id;
+// import jakarta.persistence.Table;
+// import lombok.AllArgsConstructor;
+// import lombok.Builder;
+// import lombok.Getter;
+// import lombok.NoArgsConstructor;
+// import lombok.Setter;
+
+// @Entity
+// @Getter
+// @Setter
+// @NoArgsConstructor
+// @AllArgsConstructor
+// @Builder
+// @Table(name = "users")
+// public class User {
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     private Long id;
+
+//     @Column(unique = true)
+//     private String username;
+
+//     @Column(unique = true)
+//     private String email;
+
+//     private String password;
+
+//     @ElementCollection(fetch = FetchType.EAGER)
+//     @CollectionTable(name = "user_roles")
+//     @Column(name = "role")
+//     private Set<String> roles;
+// }
+
 package com.streamit.user_service.entity;
 
 import java.util.Set;
@@ -29,13 +74,20 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     private String username;
 
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     private String email;
 
     private String password;
+
+    // New fields - can be duplicated
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
 
     @ElementCollection(fetch = FetchType.EAGER)
     @CollectionTable(name = "user_roles")
